643109104725a4de5a7573e8949b3a767d3adeeb
th4l3s-ceub <thales.marques@sempreceub.com>
2021-08-20 23:19:32 -0300

    ServletAgenda

---
+++ aula/src/main/java/aula03/ServletAgenda.java
@@ -0,0 +1,50 @@
+package aula03;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletAgenda
+ */
+@WebServlet("/ServletAgenda")
+public class ServletAgenda extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletAgenda() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+    @Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String nome = request.getParameter("nome");
+		String telefone = request.getParameter("telefone");
+		String dta_nasc = request.getParameter("data de nascimento");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.println("Bem Vindo<h3>"+nome+"; " +telefone+"; "+dta_nasc+"</h3>");
+		out.close();
+		
+	}
+
+}
